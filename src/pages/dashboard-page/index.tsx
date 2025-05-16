6bbe59387417a274ed53bcbddac95d52a0cdefa2
manish <manish.rawat@essencesoftwares.com>
2025-05-16 13:28:33 +0530

    feat: dashboard page added

--- src/pages/dashboard-page/index.tsx
+++ src/pages/dashboard-page/index.tsx
@@ -1,6 +1,9 @@
 import './style.scss'
+import Icons from '../../components/Icons'
 import SidebarComponent from '../../components/Sidebar'
 import HeaderComponent from '../../components/Header'
+import orderCompletedIcon from '../../assets/images/Orders Completed.svg'
+import quotesRequestedIcon from '../../assets/images/Quotes Requested.svg'
 const DashboardPage = () => {
     return (
         <div className='flex gap-4 h-screen'>
@@ -10,7 +13,38 @@ const DashboardPage = () => {
             <div className="right-area">
                 <HeaderComponent />
                 <div className="common-area">
-                    <h3>Common form will be here...</h3>
+                    <div className="card-area">
+                        <div className="card-inner">
+                            <div className="title-area">
+                                <img src={orderCompletedIcon} alt="" />
+                                <p>Orders Completed</p>
+                            </div>
+                            <div className="counter-area">
+                                <h4>01</h4>
+                                <Icons name='ArrowRight' />
+                            </div>
+                        </div>
+                        <div className="card-inner">
+                            <div className="title-area">
+                                <img src={quotesRequestedIcon} alt="" />
+                                <p>Quotes Requested</p>
+                            </div>
+                            <div className="counter-area">
+                                <h4>01</h4>
+                                <Icons name='ArrowRight' />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="info-area">
+                        <div className="info-inner">
+                            <h6>Your Latest Choice</h6>
+                            <p>No order placed</p>
+                        </div>
+                        <div className="info-inner">
+                            <h6>Your Latest Choice</h6>
+                            <p>No order placed</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
