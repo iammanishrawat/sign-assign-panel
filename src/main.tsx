8dd557c2137bd7890c8023e057fda68549410bf1
Manish rawat <manish.rawat@essencesoftwares.com>
2025-05-24 23:12:59 +0530

    working on common slider ui fixes

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './App.css'
-import './Common.scss'
-import './Responsive.scss'
+import './panelCommon.scss'
+import './websiteCommon.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- src/pages/web-homepage/index.tsx
+++ src/pages/web-homepage/index.tsx
@@ -1,48 +1,183 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
+import 'owl.carousel/dist/assets/owl.carousel.css'
+import 'owl.carousel/dist/assets/owl.theme.default.css'
 const slides = [
-    {
-        id: 1,
-        url: 'http://3.109.198.252/api/uploads/banner-image-jpg-1747304685302-914103296.jpg',
-        alt: 'Slide 1',
-    },
-    {
-        id: 2,
-        url: 'http://3.109.198.252/api/uploads/banner-image-jpg-1747304685302-914103296.jpg',
-        alt: 'Slide 2',
-    },
-    {
-        id: 3,
-        url: 'http://3.109.198.252/api/uploads/banner-image-jpg-1747304685302-914103296.jpg',
-        alt: 'Slide 3',
-    },
-];
+  {
+    id: 1,
+    url: 'http://3.109.198.252/api/uploads/banner-image-jpg-1747304685302-914103296.jpg',
+    alt: 'Slide 1',
+  },
+  {
+    id: 2,
+    url: 'http://3.109.198.252/api/uploads/banner-image-jpg-1747304685302-914103296.jpg',
+    alt: 'Slide 2',
+  },
+  {
+    id: 3,
+    url: 'http://3.109.198.252/api/uploads/banner-image-jpg-1747304685302-914103296.jpg',
+    alt: 'Slide 3',
+  },
+]
 const WebHomePage = () => {
-    const [currentSlide, setCurrentSlide] = useState(0);
-    useEffect(() => {
-        const timer = setInterval(() => {
-            setCurrentSlide((prev) => (prev + 1) % slides.length);
-        }, 3000);
-        return () => clearInterval(timer);
-    }, []);
-    return (
-        <>
-            <div className="relative w-full overflow-hidden">
-                <div
-                    className="flex transition-transform duration-700"
-                    style={{ transform: `translateX(-${currentSlide * 100}%)` }}
-                >
-                    {slides.map((slide) => (
-                        <img
-                            key={slide.id}
-                            src={slide.url}
-                            alt={slide.alt}
-                            className="w-full"
-                        />
-                    ))}
-                </div>
+  const [currentSlide, setCurrentSlide] = useState(0)
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentSlide((prev) => (prev + 1) % slides.length)
+    }, 3000)
+    return () => clearInterval(timer)
+  }, [])
+  return (
+    <>
+      <div className="relative w-full overflow-hidden">
+        <div
+          className="flex transition-transform duration-700"
+          style={{ transform: `translateX(-${currentSlide * 100}%)` }}
+        >
+          {slides.map((slide) => (
+            <img
+              key={slide.id}
+              src={slide.url}
+              alt={slide.alt}
+              className="w-full"
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className="container-slide-area pt-5">
+        <div className="container-fluid">
+          <div className="view-all-area">
+            <div className="left">
+              <h3 className="mb-0">Most Loved Brand Visionaries</h3>
             </div>
-        </>
-    )
+            <div className="right">
+              <div className="owl-nav">
+                <button type="button" role="presentation" className="owl-prev">
+                  <i className="fa-solid fa-arrow-left-long"></i>
+                </button>
+                <button type="button" role="presentation" className="owl-next">
+                  <i className="fa-solid fa-arrow-right-long"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div id="owlSection1" className="owl-carousel owl-theme">
+            <div className="inner-card">
+              <a href="/product/a-frame">
+                <div className="p-3 m-0">
+                  <img
+                    src="http://3.109.198.252/images/logo.png"
+                    alt="A Frame"
+                  />
+                </div>
+                <div className="px-3 mt-0">
+                  <h5>A Frame</h5>
+                  <div className="rating-area">
+                    <svg
+                      className="svg-inline--fa fa-star checked"
+                      aria-hidden="true"
+                      focusable="false"
+                      data-prefix="fas"
+                      data-icon="star"
+                      role="img"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 576 512"
+                      data-fa-i2svg=""
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+                      ></path>
+                    </svg>
+                    {/* <!-- <span className="fa fa-star checked"></span> Font Awesome fontawesome.com --> */}
+                    <svg
+                      className="svg-inline--fa fa-star checked"
+                      aria-hidden="true"
+                      focusable="false"
+                      data-prefix="fas"
+                      data-icon="star"
+                      role="img"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 576 512"
+                      data-fa-i2svg=""
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+                      ></path>
+                    </svg>
+                    {/* <!-- <span className="fa fa-star checked"></span> Font Awesome fontawesome.com --> */}
+                    <svg
+                      className="svg-inline--fa fa-star checked"
+                      aria-hidden="true"
+                      focusable="false"
+                      data-prefix="fas"
+                      data-icon="star"
+                      role="img"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 576 512"
+                      data-fa-i2svg=""
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+                      ></path>
+                    </svg>
+                    {/* <!-- <span className="fa fa-star checked"></span> Font Awesome fontawesome.com --> */}
+                    <svg
+                      className="svg-inline--fa fa-star checked"
+                      aria-hidden="true"
+                      focusable="false"
+                      data-prefix="fas"
+                      data-icon="star"
+                      role="img"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 576 512"
+                      data-fa-i2svg=""
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+                      ></path>
+                    </svg>
+                    {/* <!-- <span className="fa fa-star checked"></span> Font Awesome fontawesome.com --> */}
+                    <svg
+                      className="svg-inline--fa fa-star checked"
+                      aria-hidden="true"
+                      focusable="false"
+                      data-prefix="fas"
+                      data-icon="star"
+                      role="img"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 576 512"
+                      data-fa-i2svg=""
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"
+                      ></path>
+                    </svg>
+                    {/* <!-- <span className="fa fa-star checked"></span> Font Awesome fontawesome.com --> */}
+                  </div>
+                  <h6>
+                    Starts at:
+                    <span className="text-green">$0</span>
+                  </h6>
+                </div>
+              </a>
+              <a
+                href="http://3.109.198.252/editor"
+                target="_blank"
+                className="customized-button"
+              >
+                Customize
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default WebHomePage
