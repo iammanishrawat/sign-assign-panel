e08330f6354776b1eafe8085b313bf3ce36903f8
Manish rawat <manish.rawat@essencesoftwares.com>
2025-05-25 19:39:35 +0530

    working on slider part in homepage with swiper slider

--- src/pages/website-pages/home-page/index.tsx
+++ src/pages/website-pages/home-page/index.tsx
@@ -1,10 +1,23 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/navigation'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons'
-import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons'
+import {
+  faArrowLeftLong,
+  faArrowRightLong,
+  faStar,
+} from '@fortawesome/free-solid-svg-icons'
 import './style.scss'
 
-const slides = [
+interface Slide {
+  id: number
+  url: string
+  alt: string
+}
+
+const slides: Slide[] = [
   {
     id: 1,
     url: 'http://3.109.198.252/api/uploads/banner-image-jpg-1747304685302-914103296.jpg',
@@ -29,6 +42,8 @@ const WebHomePage = () => {
     }, 3000)
     return () => clearInterval(timer)
   }, [])
+  const prevRef = useRef<HTMLButtonElement | null>(null)
+  const nextRef = useRef<HTMLButtonElement | null>(null)
   return (
     <>
       <div className="relative w-full overflow-hidden">
@@ -47,7 +62,7 @@ const WebHomePage = () => {
         </div>
       </div>
 
-      <div className="container-slide-area">
+      {/* <div className="container-slide-area">
         <div className="container-fluid">
           <div className="view-all-area">
             <div className="left">
@@ -183,6 +198,93 @@ const WebHomePage = () => {
               </a>
             </div>
           </div>
+        </div>
+      </div> */}
+
+      <div className="container-slide-area">
+        <div className="container-fluid">
+          <div className="view-all-area">
+            <div className="left">
+              <h3>Most Loved Brand Visionaries</h3>
+            </div>
+            <div className="right">
+              <div className="slider-nav">
+                <button
+                  ref={prevRef}
+                  type="button"
+                  role="presentation"
+                  className="slider-prev"
+                >
+                  <FontAwesomeIcon icon={faArrowLeftLong} />
+                </button>
+                <button
+                  ref={nextRef}
+                  type="button"
+                  role="presentation"
+                  className="slider-next"
+                >
+                  <FontAwesomeIcon icon={faArrowRightLong} />
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <Swiper
+            modules={[Navigation]}
+            spaceBetween={20}
+            // loop
+            slidesPerView={5}
+            navigation={{
+              prevEl: prevRef.current!,
+              nextEl: nextRef.current!,
+            }}
+            onBeforeInit={(swiper) => {
+              // @ts-ignore: nextEl and prevEl will be initialized by Swiper later
+              swiper.params.navigation.prevEl = prevRef.current
+              // @ts-ignore
+              swiper.params.navigation.nextEl = nextRef.current
+            }}
+            breakpoints={{
+              320: { slidesPerView: 1 },
+              640: { slidesPerView: 2 },
+              1024: { slidesPerView: 3 },
+            }}
+          >
+            {[1, 2, 3, 4, 5, 6, 7, 8].map((item) => (
+              <SwiperSlide key={item}>
+                <div className="inner-card">
+                  <a href="/product/anniversary-banners">
+                    <div className="p-3 m-0">
+                      <img
+                        src="http://3.109.198.252/images/logo.png"
+                        alt="Anniversary Banners"
+                        className="w-full object-contain"
+                      />
+                    </div>
+                    <div className="px-3 mt-0">
+                      <h5>Anniversary Banners</h5>
+                      <div className="rating-area flex gap-1 mt-1 text-yellow-400">
+                        {[...Array(5)].map((_, i) => (
+                          <FontAwesomeIcon key={i} icon={faStar} />
+                        ))}
+                      </div>
+                      <h6>
+                        Starts at: <span className="text-green">-</span>
+                      </h6>
+                    </div>
+                  </a>
+                  <a
+                    href="http://3.109.198.252/editor"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="customized-button"
+                  >
+                    Customize
+                  </a>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </div>
     </>
